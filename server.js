cae387ab43998ebdd48acba0d56756d978483285
gabaptistella <gabrielbaptistella18@hotmail.com>
2026-03-29 13:47:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const cors = require("cors");
-app.use(cors());
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
